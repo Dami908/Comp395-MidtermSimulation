bfafbcd41fccaace10f4dd1ab309c71971872c85
Earl Denzel Perez <earldenzel@gmail.com>
2021-03-09 02:49:28 -0500

    version 2 - now we have answers for all
    car simulation complete
    organize folders

---
+++ Assets/Scripts/CarMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMover : MonoBehaviour
+{
+    private CarSpawner spawner;
+    private float speed;
+    public bool passed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner = GameObject.FindGameObjectWithTag("spawner").GetComponent<CarSpawner>();
+        speed = spawner.speed;
+        spawner.carsEntered += 1;
+        passed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zMove = Time.deltaTime * speed;
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - zMove);
+
+        if (transform.position.z < 0 && !passed)
+        {
+            passed = true;
+            spawner.carsPassed += 1;
+        }
+        if (transform.position.z < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject window;
+    public float serviceProbability; //probability that car will enter service. p
+    public float serviceSpeed; // how many minutes per car. example is 1. mean interservice time
+                               
+    private float distance; //15 units in current setup
+    public float updateTime; //equivalent to 1 min. Lets equate this to 2s for now
+    public float speed; //in cars/minute
+    public float currentTime;
+    public float minutesSinceServiceStart;
+    public int carsPassed;
+    public int carsEntered;
+
+    public Text pValueText;
+    public Text meanInterserviceTimeText;
+    public Text minutesElapsedText;
+    public Text carsEnteredText;
+    public Text carsPassedText;
+    public Text carsInSystemText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = updateTime;
+        distance = transform.position.z - window.transform.position.z;
+        speed = (distance / updateTime) / serviceSpeed;
+        minutesSinceServiceStart = 0f;
+        carsPassed = 0;
+
+        pValueText.text = "p = " + serviceProbability;
+        meanInterserviceTimeText.text = "Using " + serviceSpeed + " minutes per car";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime < 0)
+        {
+           if (Random.value < serviceProbability)
+            {
+                Instantiate(car, transform.position, Quaternion.identity);
+            }
+            currentTime = updateTime;
+            minutesSinceServiceStart += 1;
+        }
+        minutesElapsedText.text = minutesSinceServiceStart + " mins since service start";
+        carsEnteredText.text = "Cars entered: " + carsEntered;
+        carsPassedText.text = "Cars passed: " + carsPassed;
+        carsInSystemText.text = "Cars in system: " + (carsEntered - carsPassed);
+
+    }
+}
